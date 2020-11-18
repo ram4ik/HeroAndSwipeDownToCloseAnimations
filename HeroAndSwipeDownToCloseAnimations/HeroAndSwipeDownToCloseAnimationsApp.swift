52ba90f2c480c1c722948b235b43357d831b44ae
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-11-18 12:34:49 +0200

    Initial Commit

---
+++ HeroAndSwipeDownToCloseAnimations/HeroAndSwipeDownToCloseAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAndSwipeDownToCloseAnimationsApp.swift
+//  HeroAndSwipeDownToCloseAnimations
+//
+//  Created by ramil on 18.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct HeroAndSwipeDownToCloseAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
